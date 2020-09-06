bd82ed38b1129912b3477d60727d8f8ab5136a34
benn1988 <36059686+benn1988@users.noreply.github.com>
2020-09-06 14:51:19 -0700

    CSS loaders
    -> Installed style-loadr and css-loader
    -> webpack.config configuration
    -> 'style-loader' comes first and followed by 'css-loader'

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import {run} from './app/app';
 import {ComponentService} from './app/component.service';
 import {AlertService} from './app/alert.service';
+import "./main.css";
 
 const alertService = new AlertService();
 const componentService = new ComponentService();
--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,13 @@ module.exports = {
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, "dist")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/, // regex to target all css files
+                use: ["style-loader", "css-loader"] //  'style-loader' comes first and followed by 'css-loader'
+            }
+        ]
     }
-}
+};
