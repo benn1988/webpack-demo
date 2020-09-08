5ab30906ba48cdf3781d0f58281cb7af607e14fa
benn1988 <36059686+benn1988@users.noreply.github.com>
2020-09-07 22:18:07 -0700

    html-loader, file-loader and clean-webpack-plugin
    -> htm-loader to automatically require the files we reference in img tags
    -> file-loader to handle all images
    -> clean-webpack-plugin to production for a clean dist directory

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,4 @@
-const path = require("path");                // A node built-in plugin to generate the path to the files
+const path = require("path");
 var HtmlWebpackPlugin = require('html-webpack-plugin'); // Require html plugin, so that we can later use it
 
 module.exports = {
@@ -15,6 +15,21 @@ module.exports = {
                     "css-loader",   // 2. Turns css into commonjs
                     "sass-loader"   // 1. Turns sass into css
                 ]
+            },
+            {
+              test: /\.html$/,
+              use: ["html-loader"]
+            },
+            {
+              test: /\.(svg|png|jpg|gif)$/,
+              use: {
+                loader: "file-loader",
+                options: {
+                  esModule: false,
+                  name: "[name].[hash].[ext]",
+                  outputPath: "imgs"
+                }
+              }
             }
         ]
     },
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,13 @@
 const path = require("path");                // A node built-in plugin to generate the path to the files
 const common = require("./webpack.common");  // Import the common config
-const { merge } = require("webpack-merge");     // 
+const { merge } = require("webpack-merge");  // Merge webpack configs
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = merge(common, {
     mode: "production",                    // Optimize for production
     output: {
         filename: 'main.[contentHash].js', // Adding contentHash to hash an unique filename for each version, based on file content
         path: path.resolve(__dirname, "dist")
-    }
+    },
+    plugins: [new CleanWebpackPlugin()]
 });
